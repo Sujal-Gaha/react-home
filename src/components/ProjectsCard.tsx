3586fc5dcd47ac6894fc4c7fd645aec6111f76b3
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-03-04 20:22:33 +0545

    added css to the ProjectsCard

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -7,11 +7,11 @@ export function ProjectsCard() {
       <Link to={"./"} className={styles.card1}>
         Counter
       </Link>
-      <div className={styles.card2}>Todo List</div>
-      <div className={styles.card3}>Book Review</div>
-      <div className={styles.card4}>Weather App</div>
-      <div className={styles.card5}>Project#5</div>
-      <div className={styles.card6}>Project#6</div>
+      <Link to={""} className={styles.card2}>Todo List</Link>
+      <Link to={""} className={styles.card3}>Book Review</Link>
+      <Link to={""} className={styles.card4}>Weather App</Link>
+      <Link to={""} className={styles.card5}>Project#5</Link>
+      <Link to={""} className={styles.card6}>Project#6</Link>
     </div>
   );
 }
