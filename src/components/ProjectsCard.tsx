069302fdac62412bef864d79e32e70aaeacc8118
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-03-04 20:30:45 +0545

    quick fix

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -5,13 +5,23 @@ export function ProjectsCard() {
   return (
     <div className={styles.cardContainer}>
       <Link to={"./"} className={styles.card1}>
-        Counter
+        <h2>Counter</h2>
       </Link>
-      <Link to={""} className={styles.card2}>Todo List</Link>
-      <Link to={""} className={styles.card3}>Book Review</Link>
-      <Link to={""} className={styles.card4}>Weather App</Link>
-      <Link to={""} className={styles.card5}>Project#5</Link>
-      <Link to={""} className={styles.card6}>Project#6</Link>
+      <Link to={""} className={styles.card2}>
+        <h2>Todo List</h2>
+      </Link>
+      <Link to={""} className={styles.card3}>
+        <h2>Book Review</h2>
+      </Link>
+      <Link to={""} className={styles.card4}>
+        <h2>Weather App</h2>
+      </Link>
+      <Link to={""} className={styles.card5}>
+        <h2>Project#5</h2>
+      </Link>
+      <Link to={""} className={styles.card6}>
+        <h2>Project#6</h2>
+      </Link>
     </div>
   );
 }
